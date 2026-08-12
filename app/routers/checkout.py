1435f16b8dd464f7fafcb082b65987f7c336d99b
fw7th <okosa7th@gmail.com>
2026-08-12 10:03:48 +0100

    feat(checkout.py): edit endpoint, presume workability

--- app/routers/checkout.py
+++ app/routers/checkout.py
@@ -1,16 +1,12 @@
-from datetime import datetime, timedelta
-
-from fastapi import APIRouter, Depends, Form, Request, status
+from fastapi import APIRouter, Depends, Request
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
-from sqlalchemy.exc import SQLAlchemyError
 from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.database import get_session
-from app.security import password_hash
 
-from ..models import CartItem, Product, Users
+from ..models import CartItem, Product
 from ..utils import authenticate
 
 router = APIRouter(tags=["checkout"])
@@ -36,15 +32,19 @@ async def checkout(
     results = await session.exec(statement)
     cart_items = results.all()
 
+    subtotal = 0.0
+    # Each result is (CartItem, Product)
+    for cart_item, product in cart_items:
+        subtotal += cart_item.quantity * product.price
+
     return templates.TemplateResponse(
         request=request,
         name="checkout.html",
         context={
             "username": user.username if user else None,
-            #        "cart_items": cart_items,
+            "cart_items": cart_items,
             "system_bundles": [],
-            #        "total": subtotal,
-            #        "subtotal": subtotal,
-            #        "added_product": added_product,
+            "total": subtotal,
+            "subtotal": subtotal,
         },
     )
